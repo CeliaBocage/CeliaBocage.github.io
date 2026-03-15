2093422c56af35e11a867168b7746669e3059362
CeliaBocage <celia.bocage.pro@gmail.com>
2026-03-15 17:11:29 +0100

    restructuration

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 // ===== Configuration =====
-const API_BASE = 'https://celia-bocage-github-io.vercel.app/api';
+const API_BASE = '/api';
 
 // ===== Compteur de visites =====
 function trackVisit() {
